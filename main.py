96f4fde371b301f97323e8306fb31f63544601f7
Sebastian Reina <semorenore@unal.edu.co>
2022-01-13 18:26:43 -0500

    Risk matrix calculation 0.1v

--- main.py
+++ main.py
@@ -2,13 +2,29 @@
 
 # Minesweeper Agent
 from controller.pages.minesweeper_page import MinesweeperPage
+from models.minesweeper_models import *
 
 page = MinesweeperPage()
+mModel = UncoveredCellState(0)
 
 page.navigate_to_minesweeper_website()
 page.create_game()
 page.select_beginner_difficulty_level()
 page.flag_cell(2, 2)
 page.uncover_cell(5, 5)
-print(str(page.get_board_state().board))
-page.close()
+
+boardString = page.get_board_state().board
+
+boardWarnings = []
+
+for i in range(9):
+    fixedList = []
+    for y in range(9):
+        if(isinstance(boardString[i][y], type(mModel))):
+            fixedList.append(int(boardString[i][y].number))
+        else:
+            fixedList.append(9)
+    boardWarnings.append(fixedList)
+    
+print(str(boardWarnings))
+        
