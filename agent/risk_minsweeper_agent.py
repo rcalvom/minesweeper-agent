e21e0bf169d997aecaf69e2582cc67887f9b9281
Ricardo Andres Calvo Mendez <rcalvom@unal.edu.co>
2022-01-16 13:50:22 -0500

    Avances

--- agent/risk_minsweeper_agent.py
+++ agent/risk_minsweeper_agent.py
@@ -11,10 +11,10 @@ import numpy as np
 
 class RiskMinsweeperAgent(Agent):
 
-    def __init__(self, x, y, mine_factor):
-        self.x = x
-        self.y = y
-        self.mine_factor = mine_factor
+    def __init__(self):
+        self.x = int
+        self.y = int
+        self.mine_factor = int
         self.page = MinesweeperPage()
         self.is_won = bool
         self._3BV = int
@@ -24,19 +24,21 @@ class RiskMinsweeperAgent(Agent):
     def run(self):
         self.page.navigate_to_minesweeper_website()
         self.page.create_game()
-        self.page.select_custom_difficulty_level(9, 9, int(9 * 9 * self.mine_factor))
+        self.page.select_custom_difficulty_level(self.x, self.y, int(self.x * self.y * self.mine_factor))
         sleep(1)
 
         while not self.page.is_lost() and not self.page.is_won():
             nextMoves = self.calculateRisk(self.x, self.y)
             bla = randint(0, len(nextMoves)-1)
             nextMove = nextMoves[bla]
-            self.page.uncover_cell(nextMove[1], nextMove[0])
+            self.page.uncover_cell(nextMove[0], nextMove[1])
 
         if self.page.is_lost():
             self.is_won = False
         elif self.page.is_won():
             self.is_won = True
+            self._3BV = self.page.get_3BV()
+            self.clicks = self.page.get_clicks()
     
     
     def calculateRisk(self, rows, columns) -> list:
--- test3.py
+++ test3.py
@@ -1,46 +1,67 @@
-"""Test 3 - Agent
-This Agent solve minesweeper
+"""
+Test 3 - Agent
+This Agent solve minesweeper using a risk metric
 """
 
 # Minesweeper Agent
 from agent.risk_minsweeper_agent import RiskMinsweeperAgent
 
-# Matplotlib
-import matplotlib.pyplot as plt
-
-# NumPy
-import numpy as np
-
 # Pandas
 import pandas as pd
 
-mines_factor = [0.05, 0.10, 0.15]
-results_wins = []
-results_losses = []
-xlabel_mines_factor = []
-games = 20
+#sizes = [(10, 10), (12, 12), (14, 14), (16, 16), (18, 18)]
+#mines_proportions = [0.05, 0.10, 0.15, 0.20, 0.25]
+#games = 50
 
-for mine_factor in mines_factor:
-    games_won = 0
-    agent = RiskMinsweeperAgent(9,9,mine_factor)
-    for i in range(games):
-        agent.run()
-        if agent.is_won:
-            games_won += 1
-    results_wins.append(games_won)
-    results_losses.append(games - games_won)
-    xlabel_mines_factor.append('{}%'.format(mine_factor*100))
-    print("MINE FACTOR: {}".format(mine_factor))
-    print("WINS: {}".format(games_won))
-    print("LOSSES: {}".format(games - games_won))
-    agent.page.close()
+sizes = [(10, 10)]
+mines_proportions = [0.05, 0.10]
+games = 1
+
+df = pd.DataFrame(columns=[
+    "TEST",
+    "WIDTH",
+    "HEIGHT",
+    "MINES_PROPORTION",
+    "VICTORY",
+    "CLICKS",
+    "3BV",
+    "EFICIENCY"
+])
+
+agent = RiskMinsweeperAgent()
+for size in sizes:
+    for mines_proportion in mines_proportions:
+        agent.x = size[0]
+        agent.y = size[1]
+        agent.mine_factor = mines_proportion
+        for i in range(games):
+            agent.run()
+            if agent.is_won:
+                data = {
+                    "TEST": (i + 1),
+                    "WIDTH": size[0],
+                    "HEIGHT": size[1],
+                    "MINES_PROPORTION": mines_proportion,
+                    "VICTORY": True,
+                    "CLICKS": agent.clicks,
+                    "3BV": agent._3BV,
+                    "EFICIENCY": agent._3BV / agent.clicks
+                }
+                df = df.append(data, ignore_index=True)
+            else:
+                data = {
+                    "TEST": (i + 1),
+                    "WIDTH": size[0],
+                    "HEIGHT": size[1],
+                    "MINES_PROPORTION": mines_proportion,
+                    "VICTORY": False,
+                    "CLICKS": None,
+                    "3BV": None,
+                    "EFICIENCY": None
+                }
+                df = df.append(data, ignore_index=True)
+agent.page.close()
+
+df.to_excel("test3.xlsx")
 
 
-df = pd.DataFrame({
-    'Mines density': xlabel_mines_factor,
-    'Wins': results_wins,
-    'Losses': results_losses
-})
-
-df.plot(x="Mines density", y=['Wins', 'Losses'], kind='bar')
-plt.show()
